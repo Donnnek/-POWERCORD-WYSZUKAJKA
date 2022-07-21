d8a46fba4f38e8cc9c010a13a5f63ba46ba9977a
Donnnek <87273692+Donnnek@users.noreply.github.com>
2022-07-22 01:26:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ module.exports = class Wyszukajka extends Plugin {
                 let query = encodeURI(args.join(' '))
                 let treść = args.join(' ')
                 let url = `**${treść}**
-*Link: <https://wyszukajka.netlify.app/szukaj?q=${query}>*
-
-*Adblock: <https://adguard.com/pl/welcome.html>*`
+*Link: <https://wyszukajka.netlify.app/szukaj?q=${query}>*`
                 return {
                     send: true,
                     result: url
